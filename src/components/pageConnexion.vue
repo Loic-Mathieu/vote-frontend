ac77171e4ef4a0b33abc86a1f56a46eccf4075da
Loic-Mathieu <loicn.mathieu@gmail.com>
2020-01-19 03:09:00 +0100

    token
    TODO : passer les info sur l'autre page

--- src/components/pageConnexion.vue
+++ src/components/pageConnexion.vue
@@ -10,9 +10,9 @@
                         <div class="form-group row">
                             <label for="matricule" class="col-3">Identifiant</label>
                             <input
-                                name="iMatricule"
-                                v-model="matricule"
-                                id="matricule"
+                                name="iLogin"
+                                v-model="login"
+                                id="login"
                                 class="form-control col"
                                 required
                             >
@@ -43,31 +43,88 @@
         data ()
         {
             return {
-                matricule : null,
+                login : null,
                 password : null,
+
+                user: {info: null, token: null}
             }
         },
 
         methods:
         {
-            returnTrue()
+            async getCredentials()
             {
-                return true;
+                const uri = 'http://localhost:8000/api/token/';
+                const header = {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                };
+
+                await this.$http
+                    .post(uri,
+                        {
+                            'username': this.login,
+                            'password': this.password
+                        },
+                        {
+                            headers: header,
+                            crossorigin:true,
+                            crossdomain:true,
+                        }
+                    )
+                    // eslint-disable-next-line no-console
+                    .then(response => {this.user.token = response.data.access; console.log("REQUEST DATA", response.data)})
+                    // eslint-disable-next-line no-console
+                    .catch(e => console.log(e))
+            },
+
+            async getUser(username)
+            {
+                if(this.user.token == null)
+                    return;
+
+                const uri = 'http://localhost:8000/api/utilisateurs/';
+                const header = {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                    Authorization: 'Bearer '.concat(this.user.token)
+                };
+
+                await this.$http
+                    .get(uri,
+                        {
+                            headers: header,
+                            params: {username: username},
+                            crossorigin:true,
+                            crossdomain:true,
+                        }
+                    )
+                    .then(response => {this.user.info = response.data.results[0];})
+                    // eslint-disable-next-line no-console
+                    .catch(e => console.log(e))
             },
 
             /**
              *
              */
-            chekCredentials()
+            async chekCredentials()
             {
-                if(this.returnTrue)
-                {
-                    if(!this.returnTrue())
-                    {
-                        //TODO a déjà voté
-                    }
+                // Récupère un token si le mdp est bon
+                await this.getCredentials();
 
-                    this.$emit('valid:username', this.matricule);
+                if(this.user.token != null)
+                {
+                    // récupérer les infos de l'utilisateur
+                    await this.getUser(this.login);
+
+                    // vérifier si il peut voter
+                    if(this.user != null && !this.user.info.a_vote)
+                    {
+                        // eslint-disable-next-line no-console
+                        console.log("RESULTAT", this.user.info.username, this.user.info.a_vote, this.user.info.id);
+
+                        // this.$emit('valid:username', this.user);
+                    }
                 }
                 else
                 {
